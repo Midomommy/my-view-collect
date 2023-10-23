46e3618b159ec8d36c4389b851afae911f78ef96
Midomommy <tinasue165@gmail.com>
2023-10-24 01:37:43 +0800

    root file adjust

--- my-page/server.js
+++ my-page/server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 
 // 设置静态文件目录，通常是包含 Vue.js 构建后的文件的目录
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "my-page")));
 
 // 所有路由都指向 index.html，以便 Vue.js 处理路由
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "my-page/index.html"));
 });
 
 // 启动服务器并监听端口
